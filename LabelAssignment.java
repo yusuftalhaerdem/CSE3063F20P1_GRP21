153e279b0fa100d1f22f449d1f8c9a7af05fc960
Tugay <33989361+tugaysarici@users.noreply.github.com>
2020-11-23 23:16:35 +0300

    Create LabelAssignment.java

---
+++ LabelAssignment.java
@@ -0,0 +1,25 @@
+public class LabelAssignment {
+    private int instanceID;
+    private int[] labelID;
+
+
+    LabelAssignment(int instanceID, int[] labelID){
+        this.instanceID=instanceID;
+        this.labelID=labelID;
+    }
+    //set methods for attributes
+    public void setInstanceID(int id){
+        instanceID = id;
+    }
+    public void setLabelID(int id){
+        labelID[instanceID] = id;
+    }
+    //get methods for attributes
+    public int getInstanceID(){
+        return instanceID;
+    }
+    public int getLabelID(){
+        return labelID[instanceID];
+    }
+
+}
