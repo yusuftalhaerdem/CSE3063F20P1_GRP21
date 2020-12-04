677db0a597ec44f0d39477f129c62aca6b53aa3d
Fatih Emin Öge <fatiheminoge@gmail.com>
2020-12-05 01:27:56 +0300

    log messages will be written.

--- src/main/java/edu/marmara/annotator/Output.java
+++ src/main/java/edu/marmara/annotator/Output.java
@@ -1,12 +1,17 @@
 package edu.marmara.annotator;
 
+import java.nio.file.Paths;
 import java.util.*;
-import java.io.FileWriter;
+
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.ParseException;
 
 
 public class Output {
@@ -23,7 +28,7 @@ public class Output {
     int instanceCount;
 
 
-    public Output(RandomLabeling label, User user) throws IOException, ParseException {
+    public Output(RandomLabeling label, User user){
         this.label = label;
         this.user = user;
 
@@ -36,20 +41,21 @@ public class Output {
         this.lblPerIns = this.label.instanceLinkedList.get(0).getLblPerIns();
 
     }
+    @JsonPropertyOrder({ "dataset id", "dataset name", "maximum number of labels per instance", "class labels", "instances", "class label assignments", "users"})
+    public void writeToFile() throws IOException {
+        outputData outJson = createJsonObject();
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
 
-    public void writeToFile(){
-        JSONObject outJson = createJsonObject();
-
-        try (FileWriter file = new FileWriter("output2.json")) {
-            file.write(outJson.toJSONString());
-            file.flush();
+        try{
+            writer.writeValue(Paths.get("output3.json").toFile(), outJson);
 
         } catch (IOException e) {
-
+            e.printStackTrace();
         }
     }
 
-    private JSONObject createJsonObject() {
+    public outputData createJsonObject() {
 
         JSONObject outJson = new JSONObject();
         outJson.put("dataset id", this.datasetId);
@@ -57,46 +63,49 @@ public class Output {
         outJson.put("maximum number of labels per instance", this.lblPerIns);
 
 
-        List<Map<Object, Object>> list = new ArrayList<>();
+        JSONArray list1 = new JSONArray();
 
         for (int i=0;i < this.labelCount;i++){
             Map<Object, Object> elements = new HashMap<>();
             elements.put("label id",this.labelLinkedList.get(i).getLabelID());
             elements.put("label text", this.labelLinkedList.get(i).getLabelText());
-            list.add(elements);
+            list1.add(elements);
         }
-        outJson.put("class labels",list);
+        outJson.put("class labels",list1);
 
-        list = new ArrayList<>();
+        JSONArray list2 = new JSONArray();
         for(int i=0;i < this.instanceCount; i++){
             Map<Object, Object> elements = new HashMap<>();
             elements.put("id", this.instanceLinkedList.get(i).getInstanceID());
             elements.put("instance", this.instanceLinkedList.get(i).getInstanceText());
-            list.add(elements);
+            list2.add(elements);
         }
-        outJson.put("instances",list);
+        outJson.put("instances",list2);
 
-        list = new ArrayList<>();
+        JSONArray list3 = new JSONArray();
         for (int i=0; i < this.instanceLinkedList.size();i++){
             Map<Object, Object> elements = new HashMap<>();
             elements.put("instance id",this.instanceLinkedList.get(i).getInstanceID());
             elements.put("class label ids",this.instanceLinkedList.get(i).getLabels());
             elements.put("user id",this.user.getUserName());
             elements.put("datetime",this.instanceLinkedList.get(i).getDateTime());
-            list.add(elements);
+            list3.add(elements);
 
         }
-        outJson.put("class label assignments",list);
+        outJson.put("class label assignments",list3);
 
-        list = new ArrayList<>();
+        JSONArray list4 = new JSONArray();
         Map<Object, Object> elements = new HashMap<>();
         elements.put("user id",this.user.getUserID());
         elements.put("user name",this.user.getUserName());
 
-        list.add(elements);
-        outJson.put("users",list);
+        list4.add(elements);
+        outJson.put("users",list4);
 
-        return outJson;
+
+        outputData json = new outputData(this.datasetId, this.datasetName, this.lblPerIns, list1, list2, list3, list4);
+
+        return json;
 
     }
 }
---
+++ src/main/java/edu/marmara/annotator/outputData.java
@@ -0,0 +1,58 @@
+package edu.marmara.annotator;
+
+import org.json.simple.JSONArray;
+
+
+public class outputData {
+
+    private int datasetId;
+    private String datasetName;
+    private int lblPerIns;
+    private JSONArray classLabels;
+    private JSONArray instances;
+    private JSONArray labelAssignments;
+    private JSONArray users;
+
+    public outputData(int datasetId, String datasetName, int lblPerIns, JSONArray classLabels,
+                      JSONArray instances, JSONArray labelAssignments, JSONArray users){
+
+        this.datasetId = datasetId;
+        this.datasetName = datasetName;
+        this.lblPerIns = lblPerIns;
+        this.classLabels = classLabels;
+        this.instances = instances;
+        this.labelAssignments = labelAssignments;
+        this.users = users;
+    }
+
+    public int getDatasetId() {
+        return datasetId;
+    }
+
+    public String getDatasetName() {
+        return datasetName;
+    }
+
+    public int getLblPerIns() {
+        return lblPerIns;
+    }
+
+    public JSONArray getClassLabels() {
+        return classLabels;
+    }
+
+    public JSONArray getInstances() {
+        return instances;
+    }
+
+    public JSONArray getLabelAssignments() {
+        return labelAssignments;
+    }
+
+    public JSONArray getUsers() {
+        return users;
+    }
+
+    public outputData(){
+    }
+}
